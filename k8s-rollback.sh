e0a72e8bdbe8b84c9cfd06c4350bca6d020f0db3
lijunjie20240405 <166136728+lijunjie20240405@users.noreply.github.com>
2024-04-26 22:20:14 +0800

    Update k8s-rollback.sh

--- k8s-rollback.sh
+++ k8s-rollback.sh
@@ -1,5 +1,5 @@
 #回滚到上一个版本,使用k8s的版本回滚机制
-kubectl rollout undo deployment jenkins-demo -n <namespace>
+kubectl rollout undo deployment jenkins-demo -n <namespace> --to-revision=<version>
 kubectl rollout history deployment -n <namespace>
 
 
